7ed056b809e656dca6e0d6861ceeafa8da476452
Pierre <pierre.olivier.tilloy@gmail.com>
2017-06-13 10:20:35 +0200

    initial commit

---
+++ Workshop Git/src/Personne.java
@@ -0,0 +1,25 @@
+
+public class Personne {
+	public String nom = "";
+	public String prenom = "";
+	
+	public void direBonjour() {
+		System.out.println("Bonjour, je m'appelle " + this.nom + " " + this.prenom);
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+}
